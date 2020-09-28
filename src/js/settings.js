84cb7077ec888b75b510d06ae227ae00e3ea0e27
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-09-28 14:33:41 +0200

    Add ok and filter icon. Refactor common styles in common.scss. Add modal structure and styles for general modals.

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,12 +1,13 @@
-const closeDialog = (dialog) => dialog.remove();
+const closeDialog = (dialog) => dialog.parentNode.removeChild(dialog);
 
 const showDialog = (type) => {
     const notifications = document.getElementById('notifications');
     // Different img paths than html
     const newNotification = `
         <div class="notification">
+            <img class="notification__ok-icon" src="./assets/img/ok.svg" alt="Operacion correcta">
             <p class="notification__text">Card ${type} correctamente</p>
-            <img class="notification__close" src="./assets/img/close.svg" alt="Cerrar" onclick="closeDialog(this.parentElement)">
+            <img class="notification__close" src="./assets/img/close-gray.svg" alt="Cerrar" onclick="closeDialog(this.parentElement)">
         </div>
     `;
     notifications.insertAdjacentHTML('beforeend', newNotification);
