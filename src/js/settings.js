92753651f6d20c103da701e7c394722d74def140
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-09-28 08:50:00 +0200

    Add auto card remove and toggle holiday functionality

--- src/js/settings.js
+++ src/js/settings.js
@@ -1,5 +1,5 @@
 
-const closeDialog = (dialog) => dialog.style.display = 'none';
+const closeDialog = (dialog) => dialog.remove();
 
 const showDialog = (type) => {
     const notifications = document.getElementById('notifications');
@@ -9,13 +9,20 @@ const showDialog = (type) => {
             <p class="notification__text">Card ${type} correctamente</p>
             <img class="notification__close" src="./assets/img/close.svg" alt="Cerrar" onclick="closeDialog(this.parentElement)">
         </div>
-    `
-    notifications.insertAdjacentHTML('beforeend', newNotification)
+    `;
+    notifications.insertAdjacentHTML('beforeend', newNotification);
+    const createdNotification = document.getElementsByClassName('notification')[document.getElementsByClassName('notification').length - 1];
+    setTimeout( () => closeDialog(createdNotification), 5000); 
 }
 
-const duplicateCard = (card) => {
-} 
+const toggleHoliday = (holiday) => {
+    if (!holiday.classList.contains('holiday-selector__option--selected')) {
+        const previousSelected = document.getElementsByClassName('holiday-selector__option--selected')[0];
+        previousSelected.classList.remove('holiday-selector__option--selected');
+        holiday.classList.add('holiday-selector__option--selected');
+    }
+};
 
 window.closeDialog = closeDialog;
-
 window.showDialog = showDialog;
+window.toggleHoliday = toggleHoliday;
