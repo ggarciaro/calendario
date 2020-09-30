e6d200bebf633c6fb3915674a1ff84edb7076b5f
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-09-30 15:23:31 +0200

    Add table-controllers and toggleControllers functionality

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,8 +1,26 @@
+const toggleControllers = () => {
+    const checkedInputs = document.getElementsByClassName('table__body')[0].querySelector('input:checked');
+    const controllers = document.getElementById('tableControllers');
+    const dashboardContainer = document.getElementById('dashboardContainer');
+
+    if (checkedInputs) {
+        controllers.style.display = 'flex';
+        dashboardContainer.style.padding = '1rem 2rem 7rem 2rem';
+    } else {
+        controllers.style.display = 'none';
+        dashboardContainer.style.padding = '1rem 2rem';
+    }
+}
+
 const toggleCheckAll = (isChecked) => {
     const inputs = document.getElementsByClassName('table__body')[0].querySelectorAll('.custom-checkbox__input');
     if (inputs) {
-        inputs.forEach(input => input.checked = isChecked)
+        for (input of inputs) {
+            input.checked = isChecked;
+        }
     }
+    toggleControllers();
 };
 
+window.toggleControllers = toggleControllers;
 window.toggleCheckAll = toggleCheckAll;
