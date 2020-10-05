4d337c6ca5424f46010a45ce10e4c8e6ad238710
Gabriel García Rodríguez <ggarciaro@minsait.com>
2020-10-05 11:42:51 +0200

    Add toggle tab functionality

--- src/js/detail.js
+++ src/js/detail.js
@@ -1,1 +1,13 @@
 import '../styles/detail.scss';
+
+const selectTab = (id) => {
+    const tab = id.split('Tab')[0];
+
+    document.getElementsByClassName('tab--selected')[0].classList.remove('tab--selected');
+    document.getElementsByClassName('detail__display--selected')[0].classList.remove('detail__display--selected');
+
+    document.getElementById(tab + 'Tab').classList.add('tab--selected');
+    document.getElementById(tab + 'Display').classList.add('detail__display--selected');
+}
+
+window.selectTab = selectTab;
